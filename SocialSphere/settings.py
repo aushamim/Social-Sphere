7e78df5e91c1549d0b5876b73f80f0dbd0945967
Shamim <au.shamim0@gmail.com>
2024-04-14 22:48:18 +0600

    Changed Allowed Hosts

--- SocialSphere/settings.py
+++ SocialSphere/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["social-sphere-gu6t.onrender.com/"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["https://social-sphere-gu6t.onrender.com/"]
 
 
